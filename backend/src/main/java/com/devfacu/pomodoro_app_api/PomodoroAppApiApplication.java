603bee8c160619f1fddf76971a52e1717e096005
Facundo Villegas <facucba@gmail.com>
2026-08-20 21:55:55 -0300

    conf: Backend spring initialzr base files

---
+++ backend/src/main/java/com/devfacu/pomodoro_app_api/PomodoroAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.devfacu.pomodoro_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoroAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoroAppApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devfacu/pomodoro_app_api/PomodoroAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfacu.pomodoro_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
